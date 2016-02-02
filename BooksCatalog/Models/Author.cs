f98c7ccc04a3112a67533b1831a46166011de04f
Barbados87 <opacha2012@gmail.com>
2016-02-02 17:23:47 +0200

    Added annotations into model classes

--- BooksCatalog/Models/Author.cs
+++ BooksCatalog/Models/Author.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BooksCatalog.Models
 {
     public class Author
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
