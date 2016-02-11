8cd9941f8b83a6b728b6d53bf4987481f05ccbc6
Barbados87 <opacha2012@gmail.com>
2016-02-11 18:10:39 +0200

    Updated Index view with Angular logic

--- BooksCatalog/Scripts/app/controllers/controllers.js
+++ BooksCatalog/Scripts/app/controllers/controllers.js
@@ -1,7 +1,12 @@
 ﻿var booksController = new angular.module('booksController', []);
 
 booksController.controller('BookListCtrl', ['$scope', '$http',
-    function($scope, $http) {
-        
+    function ($scope, $http) {
+        $scope.title = "Prived Medved";
+        $http.get("/bookscatalog/api/books").success(function(data) {
+            $scope.books = data;
+        }).error(function(data) {
+            $scope.title = "Something went wrong...";
+        });
     }
 ]);
